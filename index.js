829a556f9f04ed8309d571abaa870460c0ad3bc8
Bradley Vasilik <vasilikb2014@gmail.com>
2019-12-24 22:36:52 -0500

    Setting up DB for mongo

---
+++ index.js
@@ -0,0 +1,6 @@
+const express = require('express') 
+const logger = require('morgan')
+const errorhandler = require('errorhandler')
+const mongodb= require('mongodb')
+const bodyParser = require('body-parser')
+
